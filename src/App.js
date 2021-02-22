27cf1ffb5f990eed6a855408b1b8eae603ddc117
Han Nguyen <hanvnguyen94@gmail.com>
2021-02-22 16:18:39 -0500

    cant find a solution for map removed map in App instead

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ import BathroomShow from './components/BathroomShow/BathroomShow'
 import BathroomUpdate from './components/BathroomUpdate/BathroomUpdate'
 
 // MapBox components
-import MapBoxContainer from './components/MapBox/MapBox'
+// import MapBoxContainer from './components/MapBox/MapBox'
 
 import Homepage from './components/Homepage/Homepage'
 
@@ -86,7 +86,7 @@ class App extends Component {
             <AuthenticatedRoute user={user} exact path='/bathrooms' render={() => (
               <div style={{ minHeight: '78vh' }}>
                 <h2 className='text-center mb-3' style={{ marginBottom: '2%' }}>All Loos</h2>
-                <MapBoxContainer />
+                {/* <MapBoxContainer /> */}
                 <BathroomIndex msgAlert={this.msgAlert} user={user} />
               </div>
             )} />
